4f87f8d5713bb5052d73688d0e4280bbe0a9d274
fabianoamaral75 <fabianoamaral.ti@gmail.com>
2025-03-20 13:52:35 -0300

    Ajuste no endpont getAllRamoAtividade

--- src/main/java/br/com/uaitagcartaoconvenio/cartaoconvenio/controller/RamoAtividadeController.java
+++ src/main/java/br/com/uaitagcartaoconvenio/cartaoconvenio/controller/RamoAtividadeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -34,7 +35,7 @@ public class RamoAtividadeController {
 	}
 
 	@ResponseBody
-	@PostMapping(value = "/getAllRamoAtividade")
+	@GetMapping(value = "/getAllRamoAtividade")
 	public ResponseEntity<List<RamoAtividade>> getAllRamoAtividade(  ) throws ExceptionCustomizada, UnsupportedEncodingException{
 
 		
